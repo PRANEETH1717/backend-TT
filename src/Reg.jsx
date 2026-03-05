917624a7e08a862f0c5725a1e49420364d5b6111
PRANEETH9866 <23eg106e32@anurag.edu.in>
2026-03-05 10:49:07 +0530

    Updated frontend

---
+++ src/Reg.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import axios from "axios"
+
+function Reg() {
+
+  const [data, setData] = useState({
+    username: "",
+    email: "",
+    password: ""
+  })
+
+  const changeName = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value })
+  }
+
+  const submit = async () => {
+    try {
+
+      const res = await axios.post(
+        "https://demoo-2.onrender.com/register",
+        data
+      )
+
+      alert(res.data)
+
+    } catch (err) {
+      alert(err.response?.data || "Error occurred")
+    }
+  }
+
+  return (
+    <>
+      <h1>Register</h1>
+
+      <input
+        onChange={changeName}
+        name="username"
+        placeholder="Enter username"
+      />
+
+      <input
+        onChange={changeName}
+        name="email"
+        placeholder="Enter email"
+      />
+
+      <input
+        onChange={changeName}
+        name="password"
+        type="password"
+        placeholder="Enter password"
+      />
+
+      <button onClick={submit}>Register</button>
+    </>
+  )
+}
+
+export default Reg
